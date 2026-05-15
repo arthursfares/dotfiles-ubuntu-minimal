eefbba4c5086dccc2f459b5b445612682be5b722
arthursfares <arthursfares@gmail.com>
2026-05-15 18:51:09 -0300

    update power.sh to show clu in icon

--- i3blocks/power.sh
+++ i3blocks/power.sh
@@ -8,7 +8,7 @@
 #    * shutdown  -> systemctl poweroff
 # ============================================================================
 
-icon="（⚆_⚆）"
+icon="$USER "
 
 case "$BLOCK_BUTTON" in
     1)  # left click — open the menu
