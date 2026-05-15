9a60532d3e45aec78a0587b66145d1eaeaddae39
arthursfares <arthursfares@gmail.com>
2026-05-15 18:52:50 -0300

    uppercase user name

--- i3blocks/power.sh
+++ i3blocks/power.sh
@@ -8,7 +8,7 @@
 #    * shutdown  -> systemctl poweroff
 # ============================================================================
 
-icon="$USER "
+icon="${USER^^} "
 
 case "$BLOCK_BUTTON" in
     1)  # left click — open the menu
